a9db325d3475b0342600a82fdc6ac354c0ba0ca5
Andrew11121 <andrew1000@list.ru>
2016-11-18 03:37:54 +0400

    Add files via upload

---
+++ takeahead.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl http://www.rbc.ru/ -o "rbc1.txt"
+grep -A 1 "news-feed__item__title" rbc1.txt >  Dry1.txt
+sed /span*/d  Dry1.txt > Last_News.txt
+#sed s/--/  Dry2.txt > Last_News.txt
+#sed /???????/d Dry2.txt > Last_News.txt 
+rm rbc1.txt Dry[1-2].txt
+open Last_News.txt 
+
