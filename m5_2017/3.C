224743ca41a08c8a1daf9cd3287b20229a62fca7
Andrew11121 <andrew1000@list.ru>
2017-03-22 23:15:52 +0300

    Update 3.C

--- m5_2017/3.C
+++ m5_2017/3.C
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+void main(void) 
+{
+	float x, y, a, b, c, d;
+	printf("Enter 2 complex numbers a+bi,x+yi\n ");
+	scanf_s("%f+%fi,%f+%fi", &x, &y, &a, &b);
+	c = (x*x*a + x*a*a + x*b*b + a*y*y) / ((x + a)*(x + a) + (y + b)*(y + b));
+	d = (x*x*b + a*a*y + y*y*b + y*b*b) / ((x + a)*(x + a) + (y + b)*(y + b));
+	printf("(%.3f)+(%.3fi)\n", c, d);
+}
