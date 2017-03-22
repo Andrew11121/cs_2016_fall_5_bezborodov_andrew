8f6f29540026ff44b0f8929e7c3630b67276c2a1
Andrew11121 <andrew1000@list.ru>
2017-03-22 23:43:14 +0300

    Update 9.C

--- m5_2017/9.C
+++ m5_2017/9.C
@@ -1,1 +1,27 @@
+#include "stdio.h"
 
+int fib(int n)
+{
+	if (n == 0)
+		return 0;
+	else if ((n == -1) || (n == 1))
+		return 1;
+	else
+	{
+		if (n > 0)
+			return fib(n - 1) + fib(n - 2);
+		else
+			return fib(n + 2) - fib(n + 1);
+	}
+}
+
+void main(void)
+
+{
+	long int n;
+	int i;
+	printf("Input n: ");
+	scanf_s("%d", &n);
+	for (i = 0; i <= n; i++)
+		printf("%d - %d\n", i, fib(i));
+}
