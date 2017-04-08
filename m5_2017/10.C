74753ed122ba284c25e5bdc89a2617d15a348866
Andrew11121 <andrew1000@list.ru>
2017-04-09 00:13:36 +0300

    Update 10.C

--- m5_2017/10.C
+++ m5_2017/10.C
@@ -1,1 +1,19 @@
+#include <stdio.h
 
+
+unsigned int rightrot(unsigned int x,unsigned int n)
+{
+	for(unsigned int i = 0; i < n; i++) 
+		__asm ror x, 1
+	return x;
+}
+
+int main()
+{
+	unsigned int x, n;
+	printf("Imput a value x and the number of rows n:");
+	scanf("%u %u", &x, &n);
+	x = rightrot(x, n);
+	printf("%u\n", x);
+	return 0;
+}
