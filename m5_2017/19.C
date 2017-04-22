ad7de2a7aa5eb473b5523002bf0ecca0b91d843a
Andrew11121 <andrew1000@list.ru>
2017-04-22 20:21:34 +0300

    Update 19.C

--- m5_2017/19.C
+++ m5_2017/19.C
@@ -4,38 +4,39 @@
 #define true 1
 #define false 0
 
+
 int main()
 {
 	bool checker1 = true;
-	FILE * sh;
-	FILE * fi;
-	char fif[255];
+	FILE * s;
+	FILE * k;
+	char fk[255];
 	for (int i = 0; i < 255; i++)
-		fif[i] = '\0';
-	printf("Imput a way to your file:");
-	gets(fif);
-	if (fif == NULL)
-		printf("Empty file\n");
-	sh = fopen(fif, "rb");
+		fk[i] = '\0';
+	printf("Imput a way to your file (Ex: C:/Folder/file.txt): ");
+	gets(fk);
+	if (fk == NULL)
+		printf("Empty file");
+	s = fopen(fk, "rb");
 	int fsize = 0, counter = 0, f = 0, index = 0, i = 0, j = 0;
 	char * buffer;
-	char * a;
+	char * d;
 
-	fseek(sh, 0, SEEK_END);
-	fsize = ftell(sh);
-	rewind(sh);
+	fseek(s, 0, SEEK_END);
+	fsize = ftell(s);
+	rewind(s);
 	buffer = (char *)malloc(fsize);
-	fread(buffer, 1, fsize, sh);
-	fclose(sh);
+	fread(buffer, 1, fsize, s);
+	fclose(s);
 	f = fsize;
-	a = (char*)malloc(fsize);
+	d = (char*)malloc(fsize);
 	for (i = 0; i < fsize; i++)
 	{
 		if (buffer[i] == '"') checker1 = !checker1;
-		if (buffer[i] == '/' && buffer[i+1] == '/' && checker1)
+		if (buffer[i] == '/' && buffer[i + 1] == '/' && checker1)
 		{
 			counter += 2;
-			for (j = i + 2; j < fsize;j++)
+			for (j = i + 2; j < fsize; j++)
 			{
 				if (buffer[j] == 0x0d && buffer[j + 1] == 0x0a)
 				{
@@ -67,14 +68,14 @@ int main()
 		}
 		else
 		{
-			a[index] = buffer[i];
+			d[index] = buffer[i];
 			index++;
 		}
 	}
-	
-	fi = fopen(fif, "w");
-	fwrite(a, 1, f, fi);
-	fclose(fi);
+
+	k = fopen(fk, "w");
+	fwrite(d, 1, f, k);
+	fclose(k);
 	free(buffer);
 	printf("Your file has been changed\n");
 	return 0;
