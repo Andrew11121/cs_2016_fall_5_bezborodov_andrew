4d476160907bd4736a8a867d261a6a650fb8d6fa
Andrew11121 <andrew1000@list.ru>
2017-04-26 21:25:15 +0300

    Update 16.C

--- m5_2017/16.C
+++ m5_2017/16.C
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <conio.h>
 
 char StrCopy(char *s, char *t, int x)
 {
@@ -32,7 +33,7 @@ int StrCmp(char * s, char * t, int x)
 
 int main()
 {
-	int x, n;
+	int x, n, run = 1, f;
 	char t[100], s[100];
 	for (n = 0; n < 100; n++)
 		t[n] = '\0';
@@ -40,12 +41,36 @@ int main()
 		s[n] = '\0';
 	printf("Input your string: ");
 	gets(t);
-	printf("Input the number of symbols n: ");
-	scanf("%i", &x);
-	StrCopy(s, t, x);
-	printf("Your string with n symbols: %s\n", s);
-	StrCat(s, t, x);
-	printf("Previous string witn n symbols added to it's end: %s\n", s);
-	printf("Comparing our previos string with n symbols with the original string, 1 - if the strings are equal: %i\n", StrCmp(s, t, x));
+	while (run)
+	{
+		printf("Choose the func you want to use:\n"
+			"0 - exit\n"
+			"1 - copy\n"
+			"2 - add symbols to the end\n"
+			"3 - compare your string and the original one\n");
+		switch (_getch())
+		{
+		case '0':
+			run = 0;
+			break;
+		case '1':
+			printf("Input the number of symbols n:");
+			scanf("%i", &x);
+			StrCopy(s, t, x);
+			printf("Your coppied string with n symbols: %s\n", s);
+			break;
+		case '2':
+			printf("Input the number of symbols n: ");
+			scanf("%i", &x);
+			StrCat(s, t, x);
+			printf("Previous string witn n symbols added to it's end: %s\n", s);
+			break;
+		case '3':
+			printf("Input the number of symbols n: ");
+			scanf("%i", &f);
+			printf("Comparing our previos string with n symbols with the original string, 1 - if the strings are equal: %i\n", StrCmp(s, t, x));
+			break;
+		}
+	}
 	return 0;
 }
