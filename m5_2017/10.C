dc09f056262a2c9f4fa20e2c767dbdea4c1b0656
Andrew11121 <andrew1000@list.ru>
2017-05-01 20:39:18 +0300

    Update 10.C

--- m5_2017/10.C
+++ m5_2017/10.C
@@ -1,21 +1,33 @@
 #include <stdio.h>
+#include <math.h>
+#define N 5
 
-
-unsigned int rightrot(unsigned int x,unsigned int n)
+void Rightrot(int *mas, int k)
 {
-    return (x>>n);
+	int i, j, tmp;
+	for (i = 1; i <= k; i++)
+	{
+		tmp = mas[N - 1];
+		for (j = N - 1; j>0; j--)
+			mas[j] = mas[j - 1];
+		mas[0] = tmp;
+	}
 }
 
 int main()
 {
-
-	unsigned int x, n;
-	printf("Input x: ");
-	scanf("%i",&x);
-	printf("Input n:");
-        scanf("%u", &n);
-	x = rightrot(x, n);
-	printf("%u", x);
-
+	int a[N] = { 0 };
+	int i, k;
+	for (i = 0; i<N; i++)
+	{
+		printf("A[%d] = ", i);
+		scanf("%d", &a[i]);
+	}
+	printf("Input naskolko sdvigat K:");
+	scanf("%i", &k);
+	Rightrot(a, k);
+	for (i = 0; i<N; i++)
+		printf("%d ", a[i]);
+	printf("\n");
 	return 0;
 }
